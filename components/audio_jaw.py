77a6ba5c31dc7c41e9cb2e33fdf2cde378ace135
314Reactor <34923578+LordofBone@users.noreply.github.com>
2023-06-24 22:02:53 +0100

    Updated audio to jaw system with logging and closing jaw after audio has played

--- components/audio_jaw.py
+++ components/audio_jaw.py
@@ -1,5 +1,7 @@
-import time
+import logging
 from threading import Thread
+
+import time
 
 import numpy as np
 import pyaudio
@@ -15,40 +17,38 @@ def play_audio(audio_file):
 class AudioJawSync:
     def __init__(self):
         self.p = pyaudio.PyAudio()
-        self.input_device = self.find_usb_microphone_device()
+        self.input_device = int(self.find_usb_microphone_device())
 
         self.servo_controller = JawController()
-        self.servo_controller.open_jaw()
-        self.analyzing = True
-        self.max_rms = 1
-        self.smoothed_rms = 0
-        self.alpha = 0.8  # Smoothing factor (between 0 and 1)
+
+        self.analyzing = False
 
         # Audio settings
         self.sample_rates = [8000, 11025, 16000, 22050, 32000, 44100, 48000, 88200, 96000, 176400, 192000, 384000]
         self.chunk_sizes = [128, 256, 512, 1024, 2048]
         self.RATE = self.find_compatible_sample_rate()  # Find lowest compatible sample rate
         self.CHUNK = self.find_compatible_chunk_size()  # Reduced chunk size
-        self.max_rms = 50  # Set the maximum RMS value to 55
+        self.max_rms = 50  # Set the maximum RMS value to 50
+        self.min_rms = 15  # Set the minimum RMS value to 15
 
         # Normalize RMS value to desired pulse width range (25 to 75)
-        self.min_pulse_width = 25
-        self.max_pulse_width = 75
+        self.min_pulse_width = self.servo_controller.close_pulse_width
+        self.max_pulse_width = self.servo_controller.open_pulse_width
 
     def find_usb_microphone_device(self):
         # List all audio devices
         for i in range(self.p.get_device_count()):
             info = self.p.get_device_info_by_index(i)
-            print(f"Device index: {info['index']} - {info['name']}")
+            logging.debug(f"Device index: {info['index']} - {info['name']}")
 
             # Check if the device name contains "USB PnP Sound Device"
             if "USB PnP Sound Device" in info['name']:
-                print("Found USB PnP Sound Device at index:", info['index'])
+                logging.debug("Found USB PnP Sound Device at index:", info['index'])
                 return info['index']
 
         # If the device was not found
-        print("USB PnP Sound Device not found")
-        return None
+        logging.debug("USB PnP Sound Device not found")
+        raise Exception("USB PnP Sound Device not found, please ensure it is plugged in.")
 
     def find_compatible_sample_rate(self):
         # Check for supported sample rates
@@ -58,10 +58,10 @@ class AudioJawSync:
                                                           input_device=self.input_device,
                                                           input_channels=1,
                                                           input_format=pyaudio.paInt16)
-                print(f"  Sample rate {rate} is supported: {is_supported}")
+                logging.debug(f"  Sample rate {rate} is supported: {is_supported}")
                 return rate
             except ValueError as err:
-                print(f"  Sample rate {rate} is NOT supported: {err}")
+                logging.debug(f"  Sample rate {rate} is NOT supported: {err}")
 
     # Check for supported chunk sizes
     def find_compatible_chunk_size(self):
@@ -78,11 +78,11 @@ class AudioJawSync:
 
                 # If we reach this line, the chunk size is supported. Close the stream and return the chunk size.
                 stream.close()
-                print(f"  Chunk size {chunk_size} is supported.")
+                logging.debug(f"  Chunk size {chunk_size} is supported.")
                 return chunk_size
             except Exception as err:
                 # This chunk size is not supported. Continue checking the next one.
-                print(f"  Chunk size {chunk_size} is NOT supported: {err}")
+                logging.debug(f"  Chunk size {chunk_size} is NOT supported: {err}")
 
     def analyze_audio(self):
         stream = self.p.open(format=pyaudio.paInt16,
@@ -101,24 +101,35 @@ class AudioJawSync:
             # Calculate RMS and clip to max RMS
             rms = min(np.sqrt(np.mean(data ** 2)), self.max_rms)
 
-            normalized_rms = rms / self.max_rms
-            pulse_width = (normalized_rms * (self.max_pulse_width - self.min_pulse_width)) + self.min_pulse_width
+            # Check if RMS exceeds the threshold
+            if rms > self.min_rms:
+                # Normalize RMS value to desired pulse width range
+                normalized_rms = rms / self.max_rms
+                pulse_width = (normalized_rms * (self.max_pulse_width - self.min_pulse_width)) + self.min_pulse_width
 
-            print(f"RMS: {rms} | Pulse width: {pulse_width}")
+                logging.debug(f"RMS: {rms} | Pulse width: {pulse_width}")
 
-            # Set pulse width
-            self.servo_controller.set_pulse_width(pulse_width)
+                # Set pulse width
+                self.servo_controller.set_pulse_width(pulse_width)
+            else:
+                logging.debug("RMS below threshold, closing jaw")
+                self.servo_controller.set_pulse_width(self.min_pulse_width)
 
             # Print processing time
             end_time = time.time()  # Record the end time
             processing_time = end_time - start_time
-            print(f"Processing time: {processing_time:.6f} seconds")
+            logging.debug(f"Processing time: {processing_time:.6f} seconds")
+
+        logging.debug("Audio, closing jaw")
+        self.servo_controller.set_pulse_width(self.min_pulse_width)
 
     def start(self, audio_file):
         # Start audio analysis in a separate thread
         audio_analysis_thread = Thread(target=self.analyze_audio)
-        audio_analysis_thread.daemon = True  # Daemon thread will exit when the main program exits
+        audio_analysis_thread.daemon = False  # Daemon thread will exit when the main program exits
         audio_analysis_thread.start()
+
+        self.analyzing = True
 
         # Play the audio file in the main thread
         play_audio(audio_file)
