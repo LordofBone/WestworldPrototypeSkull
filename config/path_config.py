d6f3faf1d30353b09c68ad7b9bde8727180acd58
314Reactor <34923578+LordofBone@users.noreply.github.com>
2023-06-25 18:38:13 +0100

    path_config.py added test.wav location

---
+++ config/path_config.py
@@ -0,0 +1,17 @@
+import os
+
+# Get the directory of the current script
+current_dir = os.path.dirname(os.path.realpath(__file__))
+
+# Build the path to the model file
+obj_path = os.path.join(current_dir, '..', 'rendering','models', 'default', 'face.obj')
+textures_dir = os.path.join(current_dir, '..', 'rendering', 'textures')
+cubemaps_dir = os.path.join(textures_dir, 'cubemaps', 'default')
+
+# Build the path to the vertex shader file
+vertex_shader_path = os.path.join(current_dir, '..', 'rendering', 'shaders', 'default', 'simple.vert')
+
+# Build the path to the fragment shader file
+fragment_shader_path = os.path.join(current_dir, '..', 'rendering', 'shaders', 'default', 'lighting_toneMapping.frag')
+
+test_audio_path = os.path.join(current_dir, '..', 'audio', 'test.wav')
