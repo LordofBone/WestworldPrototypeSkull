ba033783779d8cc45dee73482d2a77f0848aa473
314Reactor <34923578+LordofBone@users.noreply.github.com>
2023-07-01 11:56:21 +0100

    Added LED based resource monitor

---
+++ components/resource_monitor_leds.py
@@ -0,0 +1,67 @@
+import time
+import psutil
+import threading
+from inventorhatmini import InventorHATMini, NUM_LEDS
+
+
+class LedResourceMonitor:
+    UPDATES = 50  # How many times the LEDs will be updated per second
+    UPDATE_RATE = 1 / UPDATES
+    AUTO_SHOW = False  # Update all LEDs at once after they have all been set
+    INTERPOLATION_SPEED = 0.1  # Speed of interpolation (higher is faster)
+
+    def __init__(self):
+        self.board = InventorHATMini()
+        self.current_mem_percentage = 0.0
+        self.current_cpu_percentage = 0.0
+        self.stop_event = threading.Event()
+
+    def sleep_until(self, end_time):
+        time_to_sleep = end_time - time.monotonic()
+        if time_to_sleep > 0.0:
+            time.sleep(time_to_sleep)
+
+    def update_leds(self):
+        while not self.stop_event.is_set():
+            start_time = time.monotonic()
+
+            mem_info = psutil.virtual_memory()
+            target_mem_percentage = mem_info.percent / 100.0
+            self.current_mem_percentage += (
+                                                       target_mem_percentage - self.current_mem_percentage) * self.INTERPOLATION_SPEED
+
+            target_cpu_percentage = psutil.cpu_percent(interval=0.1) / 100.0
+            self.current_cpu_percentage += (
+                                                       target_cpu_percentage - self.current_cpu_percentage) * self.INTERPOLATION_SPEED
+
+            hue = 0.33 * (1.0 - self.current_cpu_percentage)
+
+            for i in range(NUM_LEDS):
+                if float(i) / NUM_LEDS <= self.current_mem_percentage:
+                    self.board.leds.set_hsv(i, hue, 1.0, self.current_mem_percentage, show=self.AUTO_SHOW)
+                else:
+                    self.board.leds.set_hsv(i, 0, 0, 0)
+
+            if not self.AUTO_SHOW:
+                self.board.leds.show()
+
+            self.sleep_until(start_time + self.UPDATE_RATE)
+
+    def start(self):
+        self.thread = threading.Thread(target=self.update_leds)
+        self.thread.start()
+
+    def stop(self):
+        self.stop_event.set()
+        self.thread.join()
+        self.board.leds.clear()
+
+
+if __name__ == "__main__":
+    led_monitor = LedResourceMonitor()
+    try:
+        led_monitor.start()
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        led_monitor.stop()
