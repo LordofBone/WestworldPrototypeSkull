3ec9803ce81a96e5e5dc40a56db6d7bba6f57cb5
314Reactor <34923578+LordofBone@users.noreply.github.com>
2023-07-02 23:03:56 +0100

    Added mic only mode test

--- tests/test_audio_jaw.py
+++ tests/test_audio_jaw.py
@@ -1,10 +1,24 @@
+import argparse
 import logging
-
 import os
 import sys
 
 # Configure logging level
 logging.basicConfig(level=logging.DEBUG)
+
+
+def test_audiofile_to_jaw():
+    # Create an instance of the class and start it
+    sync = AudioJawSync()
+    sync.start(test_audio_path)
+
+
+def test_mic_to_jaw(seconds_to_analyze=30):
+    # Create an instance of the class and start it
+    sync = AudioJawSync()
+    sync.min_rms = 10
+    sync.start(seconds_to_analyze=seconds_to_analyze)
+
 
 if __name__ == "__main__":
     # Get the current script's directory
@@ -23,6 +37,19 @@ if __name__ == "__main__":
     # Configure logging level
     logging.basicConfig(level=logging.DEBUG)
 
-    # Create an instance of the class and start it
-    sync = AudioJawSync()
-    sync.start(test_audio_path)
+    # Create an argument parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--mode", help="Select 'audiofile' for audio file test or 'mic' for mic test.")
+    parser.add_argument("--seconds", default=30, type=int, help="Specify the number of seconds to analyze for the mic "
+                                                                "test.")
+
+    # Parse the command line arguments
+    args = parser.parse_args()
+
+    # Run the selected test
+    if args.mode == "audiofile":
+        test_audiofile_to_jaw()
+    elif args.mode == "mic":
+        test_mic_to_jaw(seconds_to_analyze=args.seconds)
+    else:
+        print(f"Invalid mode selected. Please use 'audiofile' or 'mic'.")
