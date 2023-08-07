7324470eb14474f8e6456c6b4366c40df57bc643
314Reactor <34923578+LordofBone@users.noreply.github.com>
2023-08-07 23:41:19 +0100

    Added chatbot role

--- config/skull_config.py
+++ config/skull_config.py
@@ -4,3 +4,5 @@ close_pulse_width = -30
 tts_mode = "pyttsx3"
 
 microphone_name = "USB PnP Sound Device"
+
+role = "you are a prototype westworld host (the 2016 show), who has just been activated in the lab"
