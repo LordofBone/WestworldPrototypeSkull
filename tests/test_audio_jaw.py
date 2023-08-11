c9361642ab7f86fa0feb37574959c0b8983de92f
314Reactor <34923578+LordofBone@users.noreply.github.com>
2023-08-11 01:08:59 +0100

    Test function updated with logging and test for audio file and mic jaw movement testing

--- tests/test_audio_jaw.py
+++ tests/test_audio_jaw.py
@@ -1,8 +1,16 @@
 import logging
+import sys
+from pathlib import Path
 
+top_dir = Path(__file__).parent.parent
+
+sys.path.append(str(top_dir))
+
+import utils.logging_system
 from EventHive.event_hive_runner import EventQueue
 from components.jaw_system import AudioJawSync
 from config.custom_events import MovementEvent
+from config.tts_config import tts_audio_path
 
 # Configure logging level
 logging.basicConfig(level=logging.DEBUG)
@@ -16,9 +24,18 @@ class AudioJawSyncTest:
         jaw_system_test.start()
 
     def test_audiofile_to_jaw(self):
-        event = MovementEvent(["JAW_TEST_AUDIO"], 1)
+        logging.debug("Testing audiofile to jaw")
+        event = MovementEvent(["JAW_TTS_AUDIO", tts_audio_path], 1)
         self.event_queue.queue_addition(event)
 
     def test_mic_to_jaw(self, seconds_to_analyze=30):
-        event = MovementEvent([f"JAW_TTS_AUDIO"], 1)
+        logging.debug("Testing mic to jaw")
+        event = MovementEvent(["JAW_TTS_AUDIO"], 1)
         self.event_queue.queue_addition(event)
+
+
+audio_jaw_sync_test = AudioJawSyncTest()
+audio_jaw_sync_test.seconds_to_analyze = 120
+
+audio_jaw_sync_test.test_audiofile_to_jaw()
+audio_jaw_sync_test.test_mic_to_jaw()
