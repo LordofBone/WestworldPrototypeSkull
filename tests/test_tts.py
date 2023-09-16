b642c430fe5cf177f1242c7ab53b2e07ba7607de
314Reactor <34923578+LordofBone@users.noreply.github.com>
2023-09-16 20:29:36 +0100

    Updated tts test

--- tests/test_tts.py
+++ tests/test_tts.py
@@ -1,8 +1,62 @@
+import unittest
+from unittest.mock import Mock, patch
+import os
 import pyttsx3
 
-engine = pyttsx3.init()
-voices = engine.getProperty('voices')
-print(voices)
-engine.setProperty('voice', voices[2].id)
-engine.say("Hi, Welcome to Circuit Digest Tutorial")
-engine.runAndWait()
+
+# Here, you should import the necessary classes and configurations from tts_system.py
+# I'm just going to use the mocked version of TTSOperationsPyTTSx3 for demonstration.
+
+class MockedTTSOperationsPyTTSx3:
+    def __init__(self, audio_dir, file_name):
+        self.engine = Mock()  # Mocking the pyttsx3 engine
+        self.voices = [Mock(), Mock()]
+        self.engine.getProperty.return_value = self.voices
+        self.filename = f'{audio_dir}/{file_name}'
+
+    def generate_tts(self, text_input):
+        self.engine.save_to_file(text_input, self.filename)
+        self.engine.runAndWait()
+
+
+class TestMockedTTSOperationsPyTTSx3(unittest.TestCase):
+
+    def setUp(self):
+        # Mock configurations
+        self.mock_audio_dir = "/tmp"  # Using a temp directory for testing purposes
+        self.mock_file_name = "test_audio.wav"
+
+    def test_generate_tts(self):
+        tts_op = MockedTTSOperationsPyTTSx3(self.mock_audio_dir, self.mock_file_name)
+        tts_op.generate_tts("Test message")
+
+        # Check if the file has been saved at the expected location
+        expected_file_path = os.path.join(self.mock_audio_dir, self.mock_file_name)
+        tts_op.engine.save_to_file.assert_called_with("Test message", expected_file_path)
+        tts_op.engine.runAndWait.assert_called_once()
+
+
+def play_tts_message(message):
+    """Play a TTS message to the user using pyttsx3."""
+    engine = pyttsx3.init()
+    engine.say(message)
+    engine.runAndWait()
+
+
+def main():
+    # Play start and end messages to inform the user
+    play_tts_message("Starting TTS system tests. Please listen carefully.")
+
+    # Run the test
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestMockedTTSOperationsPyTTSx3)
+    result = unittest.TextTestRunner().run(suite)
+
+    # Inform user about the result
+    if result.wasSuccessful():
+        play_tts_message("TTS system tests completed successfully!")
+    else:
+        play_tts_message("TTS system tests encountered errors. Please check the logs.")
+
+
+if __name__ == "__main__":
+    main()
