9e7303a1d1a079a5fc8751c455e52bb141b65bc3
314Reactor <34923578+LordofBone@users.noreply.github.com>
2024-01-30 00:13:28 +0000

    Added Ollama config and use_history param

--- config/chattinggpt_config_template.py
+++ config/chattinggpt_config_template.py
@@ -3,3 +3,7 @@ role = "you are a prototype westworld host (the 2016 show), who has just been ac
 
 # Available options: "gpt", "ollama"
 chat_backend = "ollama"
+use_history = True
+
+# only used for 'ollama' chat backend, defaulting to one of the smallest models available
+ollama_model = "tinydolphin"
