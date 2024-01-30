8878a8d64f9025d7118b275473b229df01b3e36a
314Reactor <34923578+LordofBone@users.noreply.github.com>
2024-01-30 00:12:21 +0000

    Path config updated to set up system paths via function

--- config/path_config.py
+++ config/path_config.py
@@ -7,6 +7,14 @@ test_audio_path = Path(__file__).parent.parent / 'audio' / 'test.wav'
 
 tts_audio_path = Path(__file__).parent.parent / 'audio' / 'tts_output.wav'
 
-sys.path.append(str(test_audio_path))
+chatting_gpt_dir = Path(__file__).parent.parent / 'ChattingGPT'
 
-sys.path.append(str(tts_audio_path))
+
+def setup_paths():
+    sys.path.append(str(nix_dir))
+
+    sys.path.append(str(test_audio_path))
+
+    sys.path.append(str(tts_audio_path))
+
+    sys.path.append(str(chatting_gpt_dir))
