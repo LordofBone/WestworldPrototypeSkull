4fdcf94608560f802edb5e5d5adfefba4a15ac25
314Reactor <34923578+LordofBone@users.noreply.github.com>
2024-02-03 23:29:20 +0000

    conversation_config.py added for storing action lists

---
+++ config/conversation_config.py
@@ -0,0 +1,22 @@
+conversation_function_list = [
+    'generate_tts_bot_response',
+    'activate_jaw_audio',
+    'listen_stt',
+    'check_for_command',
+    'get_bot_engine_response',
+    'scan_mode_on',
+]
+
+demo_mode_function_list = [
+    'generate_tts_bot_response',
+    'activate_jaw_audio',
+    'scan_mode_on',
+]
+
+command_function_list = [
+    'generate_tts_bot_response',
+    'activate_jaw_audio',
+    'listen_stt',
+    'execute_command',
+    'scan_mode_on',
+]
