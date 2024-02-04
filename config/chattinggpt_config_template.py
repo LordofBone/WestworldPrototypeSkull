573fadfc306040c6e908c73d959c355baa312fd1
314Reactor <34923578+LordofBone@users.noreply.github.com>
2024-02-04 20:16:45 +0000

    Role added for override of model role

--- config/chattinggpt_config_template.py
+++ config/chattinggpt_config_template.py
@@ -1,9 +1,10 @@
 openai_api_key = "api_key_here"
-role = "you are a prototype westworld host (the 2016 show), who has just been activated in the lab"
+role = ("You are a prototype westworld host (the 2016 show), who has just been activated in the lab. Answer as the "
+        "host, the assistant, only.")
 
 # Available options: "gpt", "ollama"
 chat_backend = "ollama"
 use_history = True
 
 # only used for 'ollama' chat backend, defaulting to one of the smallest models available
-ollama_model = "tinydolphin"
+ollama_model = "westworld-prototype"
