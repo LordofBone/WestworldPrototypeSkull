7a995db59e3c3fd1b538c475e2dcbff37606febb
314Reactor <34923578+LordofBone@users.noreply.github.com>
2024-02-04 22:59:28 +0000

    Added new execute command function to command list

--- config/conversation_config.py
+++ config/conversation_config.py
@@ -16,6 +16,7 @@ demo_mode_function_list = [
 command_function_list = [
     'generate_tts_bot_response',
     'activate_jaw_audio',
+    'execute_command',
     'listen_stt',
     'command_checker',
     'scan_mode_on',
