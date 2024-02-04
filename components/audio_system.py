bc239cc953c9c97eadb5d20cf9342c0385ec9bc9
314Reactor <34923578+LordofBone@users.noreply.github.com>
2024-02-04 20:10:44 +0000

    Logging added

--- components/audio_system.py
+++ components/audio_system.py
@@ -92,7 +92,8 @@ class AudioEngine:
             'CHUNK': self.CHUNK
         }
 
-        logger.debug(f"Microphone {mic_key} set to {mic_name}, device: {input_device}, RATE: {self.RATE}, CHUNK: {self.CHUNK}")
+        logger.debug(
+            f"Microphone {mic_key} set to {mic_name}, device: {input_device}, RATE: {self.RATE}, CHUNK: {self.CHUNK}")
 
     def find_input_device(self, mic_name):
         # List all audio devices
