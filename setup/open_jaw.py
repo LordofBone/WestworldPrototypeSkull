6f2a256325483d50bdd81be5cc228c7bb11f12a2
314Reactor <34923578+LordofBone@users.noreply.github.com>
2024-02-18 14:17:26 +0000

    Added open_jaw.py to open jaw for demonstration purposes

---
+++ setup/open_jaw.py
@@ -0,0 +1,12 @@
+import sys
+from pathlib import Path
+
+top_dir = Path(__file__).parent.parent
+
+sys.path.append(str(top_dir))
+
+from components.jaw_system import JawController
+
+JawControllerInit = JawController()
+
+JawControllerInit.open_jaw(-10)
